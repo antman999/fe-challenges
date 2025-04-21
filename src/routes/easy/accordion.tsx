fe0aa29a88446574f26ddd7940be9a2aff0ff805
antman999 <anthonymendoza9922@gmail.com>
2025-04-21 14:14:48 -0400

    accordion done

--- src/routes/easy/accordion.tsx
+++ src/routes/easy/accordion.tsx
@@ -1,5 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
+import { ChevronDown, ChevronRight } from "lucide-react";
+
+const DATA = [
+  {
+    value: "html",
+    title: "HTML",
+    contents:
+      "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+  },
+  {
+    value: "css",
+    title: "CSS",
+    contents:
+      "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.",
+  },
+  {
+    value: "javascript",
+    title: "JavaScript",
+    contents:
+      "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
+  },
+];
 
 export const Route = createFileRoute("/easy/accordion")({
   component: Accordion,
@@ -19,7 +41,25 @@ function Accordion() {
   };
   return (
     <div className="p-2">
-      <h3>Welcome to Accordion!</h3>
+      {DATA.map((accordionItem) => {
+        const isExpanded = openSections.has(accordionItem.value);
+        return (
+          <div key={accordionItem.value} className="my-3 p-1">
+            <button
+              className={`text-md font-semibold w-full ${isExpanded && "text-indigo-600"}`}
+              onClick={() => handleSectionToggle(accordionItem.value)}
+            >
+              <span className="flex border-b border-b-indigo-600">
+                {accordionItem.title}
+                {isExpanded ? <ChevronDown /> : <ChevronRight />}
+              </span>
+            </button>
+            <section hidden={!isExpanded} className="mt-1">
+              {accordionItem.contents}
+            </section>
+          </div>
+        );
+      })}
     </div>
   );
 }
